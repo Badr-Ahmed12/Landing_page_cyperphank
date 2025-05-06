112b5101aad298595bb6b1a6be8e667b4bd8726f
Badr ahmed <isob83709@gmail.com>
2025-05-06 16:35:37 +0300

    Edit

---
+++ components/Components.js
@@ -0,0 +1,12 @@
+import './Header.js';
+import './Hero.js';
+import './aboutUs.js';
+import './Services.js';
+import './portofolio.js';
+import './Testimonials.js';
+import './Blog.js';
+import './GetInTouch.js';
+import './Footer.js';
+import './ScrollToTop.js';
+
+
